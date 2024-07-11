5dfa5f3340b50615edb1396348f29e4b62e8fc30
e-goat <gorgonaly@gmail.com>
2024-07-11 15:30:34 +0300

    Flowbite integration into Livewire environment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,16 @@
 import './bootstrap';
 import '../css/app.css'; 
 import './main';
-import 'flowbite';
+import { initFlowbite } from 'flowbite';
+
+Livewire.hook('commit', ({ component, commit, respond, succeed, fail }) => {
+  succeed(({ snapshot, effect }) => {
+      queueMicrotask(() => {
+          initFlowbite();
+      })
+  })
+})
+
+document.addEventListener('livewire:navigated', () => {
+  initFlowbite();
+})
