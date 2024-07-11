f27691da06a79f80018abdd72459a152b7b0cda5
e-goat <gorgonaly@gmail.com>
2024-07-11 20:55:04 +0300

    Single page - styles improvements

--- resources/views/livewire/posts/show-posts.blade.php
+++ resources/views/livewire/posts/show-posts.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         @foreach ($posts as $post)
             @php $slug = $post->slug; @endphp
             <livewire:posts.post :$post :key="$post->id" :$slug>
